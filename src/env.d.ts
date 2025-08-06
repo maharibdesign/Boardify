fd532b472b800a4807359013d4d0ac195b3e29c9
maharibdesigns <berekettsparkpro@gmail.com>
2025-08-06 10:28:30 +0300

    fix: add type definitions for env variables to fix TS errors

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,15 @@
 /// <reference path="../.astro/types.d.ts" />
+interface ImportMetaEnv {
+  readonly PASSWORD: string;
+  readonly PUBLIC_SUPABASE_URL: string;
+  readonly PUBLIC_SUPABASE_ANON_KEY: string;
+  readonly PUBLIC_TWA_ANALYTICS_KEY: string;
+  readonly TELEGRAM_BOT_TOKEN: string;
+  readonly VERCEL_URL: string;
+
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
